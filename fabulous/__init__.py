121c7d330715399c45cfc764939c90da037882d9
Vikeshkumar Tiwari <tvicky002@gmail.com>
2017-09-19 15:38:10 +0530

    added init for module

--- fabulous/__init__.py
+++ fabulous/__init__.py
@@ -1,1 +1,2 @@
-__author__='vikesh'
+from .fabulous import main, FakeServer, init_services, init_db, InvalidServiceDirectory, handle_message, handle_event, run_hook, loop
+from .fakeserver import FakeSlack
