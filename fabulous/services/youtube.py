21429f67cba3f728f18318be56d23074cfaddffa
buffer0x7cd <manvendra021997@gmail.com>
2018-01-18 19:23:59 +0530

    fixed issue 70

--- fabulous/services/youtube.py
+++ fabulous/services/youtube.py
@@ -1,24 +1,13 @@
 """~yt <search term> will return the top search result """
 
-try:
-    from urllib import quote
-except ImportError:
-    from urllib.request import quote
 import re
 import requests
 from random import shuffle
 
-
-
-
-
-def yt(searchterm,unsafe=False):
-    searchterm = quote(searchterm)
-    safe = "&safe=" if unsafe else "&safe=active"
-    url = "https://www.youtube.com/results?search_query={0}".format(searchterm,safe)
-    url = url.format(quote(searchterm))
-
-    r = requests.get(url)
+YT_BASEURL = "https://www.youtube.com/results?"
+def yt(searchterm):
+    payload = {'search_query': searchterm}
+    r = requests.get(YT_BASEURL, params=payload)
     results = re.findall('a href="(/watch[^&]*?)"', r.text)
 
     if not results:
