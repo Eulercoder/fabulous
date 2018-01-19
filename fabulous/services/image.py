d4cd933a8bff3b41466b98820e7bc2f3726a2fab
buffer0x7cd <manvendra021997@gmail.com>
2018-01-19 13:35:30 +0530

    Fixed urllib.quote uses in image service

--- fabulous/services/image.py
+++ fabulous/services/image.py
@@ -8,22 +8,19 @@ import re
 import requests
 from random import shuffle
 
-
+IMG_BASEURL = "https://www.google.com/search?"
 def unescape(url):
     # for unclear reasons, google replaces url escapes with \x escapes
     return url.replace(r"\x", "%")
 
 
 def img(searchterm, unsafe=False):
-    searchterm = quote(searchterm)
-
-    safe = "&safe=" if unsafe else "&safe=active"
-    searchurl = "https://www.google.com/search?tbm=isch&q={0}{1}".format(searchterm, safe)
-
+    payload = {'tbm':'isch', 'q':searchterm}
+    
     # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
 
-    result = requests.get(searchurl, headers={"User-agent": useragent}).text
+    result = requests.get(IMG_BASEURL,payload, headers={"User-agent": useragent}).text
 
     images = list(map(unescape, re.findall(r"var u='(.*?)'", result)))
     shuffle(images)
